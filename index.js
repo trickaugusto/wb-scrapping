32a90192f02702ee9a92f845c5cf44b236d6a454
trickaugusto <trickaugusto1206@gmail.com>
2024-06-21 18:01:13 -0300

    teste

--- index.js
+++ index.js
@@ -2,12 +2,28 @@ const puppeteer = require('puppeteer');
 
 (async () => {
   // Inicia o navegador
-  const browser = await puppeteer.launch({ headless: false }); // Defina headless: true para rodar sem interface gráfica
+  const browser = await puppeteer.launch({
+    headless: false, // Executa o navegador de maneira visível
+    slowMo: 100 // Desacelera cada ação em 100ms
+  });
   const page = await browser.newPage();
   
   // Navega até o site desejado
-  await page.goto('https://www.andrelsmelo.tech/');
+  await page.goto('https://www.andrelsmelo.tech/login');
+  await page.waitForSelector('input[type="text"].text-black.px-4.py-2.mt-4.rounded-s-lg.border');
+  await page.type('input[type="text"].text-black.px-4.py-2.mt-4.rounded-s-lg.border', 'trick');
+  await page.type('input[type="text"].text-black px-4 py-2 mt-4 border', '41992768209');
 
+  await page.waitForSelector('button.px-4.py-2.rounded-lg.hover\\:scale-105.transition-all.duration-400.bg-green-600.hover\\:bg-green-700.text-white.rounded-s-none.rounded-e-lg.border.border-green-600');
+  await page.click('button.px-4.py-2.rounded-lg.hover\\:scale-105.transition-all.duration-400.bg-green-600.hover\\:bg-green-700.text-white.rounded-s-none.rounded-e-lg.border.border-green-600');
+
+  setTimeout(() => {
+    console.log('Aguardando...');
+  }, 2000);
+
+  await browser.close();
+
+  return;
   // Interage com o site
   // Clica em um botão
   await page.evaluate((buttonText) => {
